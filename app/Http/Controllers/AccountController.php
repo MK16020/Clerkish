306f8638f1c5aab02e81ead3f96a5199c5e69599
MK16020 <61877101+MK16020@users.noreply.github.com>
2021-05-04 14:15:37 +0300

    controllers base functions

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+    public function index()
+    {
+        //
+    }
+
+    public function insert(Request $request)
+    {
+        //
+    }
+
+    public function update(Request $request)
+    {
+        //
+    }
+
+    public function show($ID)
+    {
+        //
+    }
+
+    public function delete($ID)
+    {
+        //
+    }
 }
