66728a05c772231e65d65ab150fab1664e9d708f
MK16020 <61877101+MK16020@users.noreply.github.com>
2021-05-06 14:28:15 +0300

    controllers

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,32 +2,175 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
-{
+{ 
     public function index()
     {
-        //
+        $account = Account::all();
+
+        return parent::getPaginatedResopnse(
+            __('accountMessages.index'),
+            $account
+        );
     }
 
     public function insert(Request $request)
-    {
-        //
+    { 
+        $request->validate([
+            'accountCode' => 'required|numeric',
+            'name' => 'required',
+            'type' => 'required',
+            'main' => 'required',
+        ]);
+
+        if (!array_key_exists(env('FALLBACK_LOCALE'), $request->name)) {
+            return parent::getResponse(
+                __('LanguageMessages.mainLanguageNotAssigned'),
+                422
+            );
+        }
+
+        $account = new account;
+
+        $account->name_j = $request->name;
+
+        if (!$account->save()) {
+            return parent::getResponse(
+                __('accountMessages.notInserted', ['name' => $request->name]),
+                304,
+                $account
+            );
+        }
+
+        return parent::getResponse(
+            __('accountMessages.inserted', ['name' => $account->name]),
+            201,
+            $account
+        );
     }
 
     public function update(Request $request)
     {
-        //
+        $request->validate([
+            'accountID' => 'required',
+            'accountCode' => 'required|numeric',
+            'name' => 'required',
+            'type' => 'required',
+            'main' => 'required',
+        ]);
+
+        if (!array_key_exists(env('FALLBACK_LOCALE'), $request->name)) {
+            return parent::getResponse(
+                __('LanguageMessages.mainLanguageNotAssigned'),
+                422
+            );
+        }
+
+        $account = Account::find($request->accountID);
+
+        if (!$account) {
+            return parent::getResponse(
+                __('accountMessages.notFound'),
+                404
+            );
+        }
+
+        $isDirty = false;
+
+        foreach ($request->name as $key => $localName) {
+            if (
+                !$account->name_j->offsetExists($key) ||
+                $localName != $account->name_j[$key]
+            ) {
+                $account->name_j[$key] = $localName;
+                $isDirty = true;
+            }
+        }
+
+        if (!$isDirty) {
+            return parent::getResponse(
+                __('accountMessages.noUpdates'),
+                200
+            );
+        }
+
+        if (!$account->save()) {
+            return parent::getResponse(
+                __('accountMessages.notUpdated', ['name' => $request->name]),
+                304,
+                $account
+            );
+        }
+
+        return parent::getResponse(
+            __('accountMessages.updated', ['name' => $account->name]),
+            200,
+            $account
+        );
     }
 
-    public function show($ID)
+    public function show($accountID)
     {
-        //
+        $account = Account::find($accountID);
+
+        if (!$account) {
+            return parent::getResponse(
+                __('accountMessages.notFound'),
+                404
+            );
+        }
+        $account = ViewaccountResource::collection([$account]);
+
+        return parent::getResponse(
+            __('accountMessages.show'),
+            200,
+            $account[0]
+        );
     }
 
-    public function delete($ID)
+    public function delete($accountID)
     {
-        //
+        $account = Account::find($accountID);
+
+        if (!$account) {
+            return parent::getResponse(
+                __('accountMessages.notFound'),
+                404
+            );
+        }
+
+        if (!$account->delete()) {
+            return parent::getResponse(
+                __('accountMessages.notDeleted'),
+                304
+            );
+        }
+
+        return parent::getResponse(
+            __('accountMessages.deleted'),
+            200
+        );
+    }
+
+    public function updateCache(Request $request)
+    {
+        if (!$request->date) {
+            $accounts = Account::all();
+        } else {
+            $accounts = Account::withTrashed()
+                ->where('created_at', '>=', $request->date)
+                ->orWhere('updated_at', '>=', $request->date)
+                ->orWhere('deleted_at', '>=', $request->date)->get();
+        }
+
+        $accounts = GeneralCacheResource::collection($accounts);
+
+        return parent::getResponseWithOutMessage(
+            200,
+            $accounts
+        );
     }
 }
--- app/Http/Controllers/DailyJournalController.php
+++ app/Http/Controllers/DailyJournalController.php
@@ -6,28 +6,170 @@ use Illuminate\Http\Request;
 
 class DailyJournalController extends Controller
 {
+  
     public function index()
     {
-        //
+        $journal = Journal::all();
+
+        return parent::getPaginatedResopnse(
+            __('journalMessages.index'),
+            $journal
+        );
     }
 
     public function insert(Request $request)
-    {
-        //
+    { 
+        $request->validate([
+            'number' => 'required',
+            'type' => 'required',
+            'date' => 'required',
+        ]);
+
+        if (!array_key_exists(env('FALLBACK_LOCALE'), $request->name)) {
+            return parent::getResponse(
+                __('LanguageMessages.mainLanguageNotAssigned'),
+                422
+            );
+        }
+
+        $journal = new journal;
+
+        $journal->name_j = $request->name;
+
+        if (!$journal->save()) {
+            return parent::getResponse(
+                __('journalMessages.notInserted', ['name' => $request->name]),
+                304,
+                $journal
+            );
+        }
+
+        return parent::getResponse(
+            __('journalMessages.inserted', ['name' => $journal->name]),
+            201,
+            $journal
+        );
     }
 
     public function update(Request $request)
     {
-        //
+        $request->validate([
+            'journalID' => 'required',
+            'number' => 'required',
+            'type' => 'required',
+            'date' => 'required',
+        ]);
+
+        if (!array_key_exists(env('FALLBACK_LOCALE'), $request->name)) {
+            return parent::getResponse(
+                __('LanguageMessages.mainLanguageNotAssigned'),
+                422
+            );
+        }
+
+        $journal = Journal::find($request->journalID);
+
+        if (!$journal) {
+            return parent::getResponse(
+                __('journalMessages.notFound'),
+                404
+            );
+        }
+
+        $isDirty = false;
+
+        foreach ($request->name as $key => $localName) {
+            if (
+                !$journal->name_j->offsetExists($key) ||
+                $localName != $journal->name_j[$key]
+            ) {
+                $journal->name_j[$key] = $localName;
+                $isDirty = true;
+            }
+        }
+
+        if (!$isDirty) {
+            return parent::getResponse(
+                __('journalMessages.noUpdates'),
+                200
+            );
+        }
+
+        if (!$journal->save()) {
+            return parent::getResponse(
+                __('journalMessages.notUpdated', ['name' => $request->name]),
+                304,
+                $journal
+            );
+        }
+
+        return parent::getResponse(
+            __('journalMessages.updated', ['name' => $journal->name]),
+            200,
+            $journal
+        );
     }
 
-    public function show($ID)
+    public function show($journalID)
     {
-        //
+        $journal = Journal::find($journalID);
+
+        if (!$journal) {
+            return parent::getResponse(
+                __('journalMessages.notFound'),
+                404
+            );
+        }
+        $journal = ViewjournalResource::collection([$journal]);
+
+        return parent::getResponse(
+            __('journalMessages.show'),
+            200,
+            $journal[0]
+        );
     }
 
-    public function delete($ID)
+    public function delete($journalID)
     {
-        //
+        $journal = Journal::find($journalID);
+
+        if (!$journal) {
+            return parent::getResponse(
+                __('journalMessages.notFound'),
+                404
+            );
+        }
+
+        if (!$journal->delete()) {
+            return parent::getResponse(
+                __('journalMessages.notDeleted'),
+                304
+            );
+        }
+
+        return parent::getResponse(
+            __('journalMessages.deleted'),
+            200
+        );
+    }
+
+
+    public function updateCache(Request $request)
+    {
+        if (!$request->date) {
+            $companies = Journal::all();
+        } else {
+            $companies = Journal::withTrashed()
+                ->where('created_at', '>=', $request->date)
+                ->orWhere('updated_at', '>=', $request->date)
+                ->orWhere('deleted_at', '>=', $request->date)->get();
+        }
+
+        $companies = GeneralCacheResource::collection($companies);
+
+        return parent::getResponseWithOutMessage(
+            200,
+            $companies
+        );
     }
 }
