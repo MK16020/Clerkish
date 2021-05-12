ffdfb1a903c3af93ebbca13225d55a716f96350e
MK16020 <61877101+MK16020@users.noreply.github.com>
2021-05-12 04:02:13 +0300

    user controler

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,6 +17,30 @@ class UserController extends Controller
         );
     }
 
+    public function insert(Request $request) //add new record
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'role' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user =  new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->role = $request->role;
+        $user->password = $request->password;
+        $user->createdBy = auth()->user()->id;
+
+        if (!$user->save()) {
+            return parent::getResponse(
+                __('accountMessages.cannotInsert'), 304);
+        }
+
+        return response()->json($user, 201);
+    }
+
     public function update(Request $request) //update specific record
     {
         $user = User::find($request->userID);
@@ -27,10 +51,10 @@ class UserController extends Controller
                404);
         }
 
-        $user->userName = $request->userName;
+        $user->name = $request->name;
         $user->email = $request->email;
-        $user->userSateID = $request->userSateID;
-        $user->userSateID = $request->userSateID;
+        $user->role = $request->role;
+        $user->password = $request->password;
 
         if (!$user->isDirty()) {
             return parent::getResponse(
@@ -42,7 +66,7 @@ class UserController extends Controller
 
         if (!$user->save()) {
            return parent::getResponse(
-                __('accountMessages.cannotUpdate'),, 304);
+                __('accountMessages.cannotUpdate'), 304);
         }
 
         return response()->json($user, 200);
@@ -67,112 +91,22 @@ class UserController extends Controller
         );
     }
 
-    public function delete($ID)
-    {
-        //
-    }
-}
-/*
-
-    
-   public function update(Request $request)
-    {
-        $request->validate([
-            'userID' => 'required',
-            'name' => 'required',
-            'phone' => 'required',
-            'role' => [
-                'required',
-                Rule::in(['admin', 'ADMIN', 'supervisor', 'SUPERVISOR', 'CASHIER', 'cashier']),
-            ],
-            'branchID' => 'required'
-        ]);
-
-        $user = User::find($request->userID);
-
-        if (!$user) {
-            return parent::getResponse(
-                __('userMessages.notFound'),
-                404
-            );
-        }
-
-        if ($user->phone != $request->phone) {
-            $isPhoneTaken = User::where('phone', $request->phone)->count();
-
-            if ($isPhoneTaken) {
-                return parent::getResponse(
-                    __('userMessages.phoneIsUsed'),
-                    405
-                );
-            }
-        }
-
-   
-
-    public function insert(Request $request) //add new record
-    {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'password' => 'required'
-        ]);
-
-        $user =  new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = bcrypt($request->password);
-        $user->userSateID = $request->userSateID;
-        $user->userTypeID = $request->userTypeID;
-        $user->createdBy = auth()->user()->id;
-
-        if (!$user->save()) {
-            return response()->json(['massage' => 'cant insert the user', $request], 304);
-        }
-
-        return response()->json($user, 201);
-    }
-
-
-    public function update(Request $request) //update specific record
-    {
-        $user = User::find($request->userID);
-
-        if (!$user) {
-            return response()->json(['massage' => 'user not found'], 404);
-        }
-
-        $user->userName = $request->userName;
-        $user->email = $request->email;
-        $user->userSateID = $request->userSateID;
-        $user->userSateID = $request->userSateID;
-
-        if (!$user->isDirty()) {
-            return response()->json(['massage' => 'nothing to update', $user], 200);
-        }
-
-        $user->udpatedBy = auth()->user()->id;
-
-        if (!$user->save()) {
-            return response()->json(['massage' => 'cant update the user', $request], 304);
-        }
-
-        return response()->json($user, 200);
-    }
-
     public function delete($userID) //delete specific record
     {
         $user = User::find($userID);
 
         if (!$user) {
-            return response()->json(['massage' => 'user not found'], 404);
+             return parent::getResponse(
+            __('userMessages.notFound'), 404);
         }
 
         if (!$user->delete()) {
-            return response()->json(['massage' => 'cant delete the user', $userID], 304);
+            return parent::getResponse(
+                __('userMessages.deleted'), 304, $userID);
         }
 
-        return response()->json(['massage' => 'user deleted'], 200);
+         return parent::getResponse(
+            __('userMessages.deleted'), 200);
     }
 
     public function login(Request $request)
@@ -185,18 +119,19 @@ class UserController extends Controller
         $credentials = $request('email','password');
 
         if(!User::attempt($credentials)){
-            return response()->json(['massage' => 'your password or email not correct'], 401);
+            return parent::getResponse(
+                __('userMessages.wrongInfo'), 401);
         }
         $token = $user->createToken('token-name');
 
-        return response()->json(['token' => $token->plainTextToken ,'massage' => 'successful login'], 200);
+        return parent::getResponse(['token' => $token->plainTextToken ,__('userMessages.logedIn')], 200);
     }
-
 
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
-        return response()->json(['massage' => 'successful logout']);
+        return parent::getResponse(
+            __('userMessages.logedout');
     }
 
     public function singup(Request $request)
@@ -211,23 +146,12 @@ class UserController extends Controller
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
-        $user->userSateID = 'notactive';
-        $user->userTypeID = 'users';
 
         if (!$user->save()) {
-            return response()->json(['massage' => 'cant insert the user', $request], 304);
+            return parent::getResponse(
+                __('userMessages.notInserted'), 304, $userID);
         }
 
         return response()->json($user, 201);
     }
-
-    public function resetPassword(Request $request)
-    {
-
-    }
-
-    public function varifyEmail($token)
-    {
-
-    }
- */
+}
