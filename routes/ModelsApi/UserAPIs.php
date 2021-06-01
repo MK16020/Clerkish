4d9153935859c859ec8d73f8dec976b618ea625c
MK16020 <61877101+MK16020@users.noreply.github.com>
2021-06-01 20:15:17 +0300

    Update UserAPIs.php

--- routes/ModelsApi/UserAPIs.php
+++ routes/ModelsApi/UserAPIs.php
@@ -9,6 +9,6 @@ Route::group(['prefix' => 'users'], function () {
         Route::post('insert', [UserController::class, 'insert']);
         Route::put('update', [UserController::class, 'update']);
         Route::get('show/{userID}', [UserController::class, 'show']);
-        Route::delete('delete/{userID}', [UserController::class, 'delete']);
+        Route::delete('delete/{userID}', [UserController::class, 'delete']);//login
         });
     });
